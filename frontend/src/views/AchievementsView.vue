8e5928e9c26d0bcc48a7401e9794f231b81fab6b
Lukas <lukiza2002@gmail.com>
2023-11-05 14:19:15 +0100

    Fixed and finished achievements screen

--- frontend/src/views/AchievementsView.vue
+++ frontend/src/views/AchievementsView.vue
@@ -1,25 +1,46 @@
+<template>
+  <v-container class="achievements-container" fluid>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="achievements-header">{{achievementsData.header}}</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-list>
+          <v-list-item-group>
+            <v-list-item v-for="item in achievementsData.data" :key="item.id">
+              <v-list-item-avatar>
+                <img :src="item.image" alt="Achievement Image" class="achievement-image">
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ item.points }} points</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
 <script setup>
 import { ref } from 'vue'
-import { data } from '@/assets/achievementsData.json'
+import achievementsData from '@/assets/achievementsData.json'
 
-const achievement = ref(null)
-async function getAchievments() {
-  await console.log(data)
-  achievement.value = data
-}
-
-await getAchievments()
+const achievements = ref(achievementsData)
 </script>
 
-<template>
-  <h1>Achievements</h1>
-  <v-list>
-    <v-list-item-group>
-      <v-list-item v-for="item in achievement" :key="item.id">
-        <v-list-item-content>
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
-</template>
+<style scoped>
+.achievements-header {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+.achievement-image {
+  width: 100px;
+  height: 100px;
+}
+</style>
