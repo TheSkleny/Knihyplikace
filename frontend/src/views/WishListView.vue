4cdb18762a0f1a03f8d631fe01031dc617c22b03
Lukas <lukiza2002@gmail.com>
2023-11-05 15:18:54 +0100

    Implemented wish list screen

--- frontend/src/views/WishListView.vue
+++ frontend/src/views/WishListView.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="wish-list">
-    <h1>Wish list</h1>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="wishlist-header">Wish list</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-list>
+            <v-list-item v-for="book in books" :key="book.id">
+              <v-list-item-content>
+                <v-list-item-title class="text-h5">{{ book.Name }}</v-list-item-title>
+                <v-list-item-subtitle class="text-body-1">{{ book.Author }}</v-list-item-subtitle>
+                <v-list-item-subtitle class="text-body-1">{{ book.BookList?.Name || '' }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const books = ref([])
+
+async function getWishListBooks() {
+  const { data, error } = await supabase
+    .from('Book')
+    .select('Name, Author, BookList!inner(*)')
+    .eq('BookList.Name', 'V seznamu přání')
+  if (error) console.log('error', error)
+  else books.value = data
+  return books.value
+}
+
+onMounted(() => {
+  getWishListBooks()
+})
+</script>
+
+<style scoped>
+.wishlist-header {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+</style>
