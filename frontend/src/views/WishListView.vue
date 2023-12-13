3d303d8fa202faa3d499cfdca15f189255f9e16b
Lukas <lukiza2002@gmail.com>
2023-12-13 19:30:08 +0100

    WishList view added

--- frontend/src/views/WishListView.vue
+++ frontend/src/views/WishListView.vue
@@ -7,20 +7,8 @@ const books = ref([])
 
 async function getWishListBooks() {
   const {data, error} = await supabase
-      .from('BookInBookList')
-      .select(`
-        Book (
-          Id,
-          Name,
-          Author,
-          CoverImageLink
-        ),
-        BookList (
-          Name
-        )
-      `)
-      .eq('BookList.Name', 'V seznamu přání')
-
+      .from('vWishListBooks')
+      .select('Id, Name, Author, CoverImageLink')
   if (error) {
     console.log('error', error)
   }
@@ -33,37 +21,6 @@ getWishListBooks()
 </script>
 
 <template>
-  <!--  <div>-->
-  <!--    <v-container>-->
-  <!--      <v-row>-->
-  <!--        <v-col>-->
-  <!--          <h1 class="wishlist-header">Wish list</h1>-->
-  <!--        </v-col>-->
-  <!--      </v-row>-->
-  <!--      <v-row>-->
-  <!--        <v-col>-->
-  <!--          <v-list>-->
-  <!--            <v-list-item v-for="book in books" :key="book.id">-->
-  <!--                <v-list-item-title class="text-h5">{{ book.Name }}</v-list-item-title>-->
-  <!--                <v-list-item-subtitle class="text-body-1">{{ book.Author }}</v-list-item-subtitle>-->
-  <!--                <v-list-item-subtitle class="text-body-1">{{-->
-  <!--                  book.BookList?.Name || ''-->
-  <!--                }}</v-list-item-subtitle>-->
-  <!--            </v-list-item>-->
-  <!--          </v-list>-->
-  <!--        </v-col>-->
-  <!--      </v-row>-->
-  <!--    </v-container>-->
-  <!--  </div>-->
-
-
-
- <WishListBookCard v-for="book in books" :key="book.Id" :book="book" />
+  <WishListBookCard v-for="book in books" :key="book.Id" :book="book" />
   <div style="display: block; height: 100px;"/>
-
-
 </template>
-
-
-<style scoped>
-</style>
